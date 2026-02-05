f20eca4ba329d3643d266fd282ecb33d3d89f349
Jaime Gonzalez Aguilar <jaimegag@gmail.com>
2026-02-04 23:34:08 -0500

    Added Runtime info to Actuator and the UI

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -8,12 +8,16 @@ createApp({
             error: null,
             cfenvInfo: null,
             cfenvLoading: false,
-            cfenvError: null
+            cfenvError: null,
+            actuatorInfo: null,
+            actuatorLoading: false,
+            actuatorError: null
         };
     },
     mounted() {
         this.fetchHello();
         this.fetchCfEnv();
+        this.fetchActuatorInfo();
     },
     methods: {
         async fetchHello() {
@@ -48,6 +52,21 @@ createApp({
                 this.cfenvError = 'Failed to reach /cfenv endpoint: ' + err.message;
             } finally {
                 this.cfenvLoading = false;
+            }
+        },
+        async fetchActuatorInfo() {
+            this.actuatorLoading = true;
+            this.actuatorError = null;
+            try {
+                const response = await fetch('/actuator/info');
+                if (!response.ok) {
+                    throw new Error(`HTTP ${response.status}`);
+                }
+                this.actuatorInfo = await response.json();
+            } catch (err) {
+                this.actuatorError = 'Failed to reach /actuator/info endpoint: ' + err.message;
+            } finally {
+                this.actuatorLoading = false;
             }
         }
     }
